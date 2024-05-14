d0d6f129b575f2d62e0dc93601e8e819fab47426
Austin Chang <austin.chang@belkin.com>
2024-05-14 11:01:35 +0800

    Add AppExpansionCard

---
+++ lib/widgets/card/expansion_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:linksys_widgets/widgets/card/card.dart';
+import 'package:linksys_widgets/widgets/text/app_text.dart';
+
+class AppExpansionCard extends StatefulWidget {
+  final String title;
+  final IconData? expandedIcon;
+  final IconData? collapsedIcon;
+  final List<Widget>? children;
+
+  const AppExpansionCard({
+    super.key,
+    required this.title,
+    required this.children,
+    this.expandedIcon,
+    this.collapsedIcon,
+  });
+
+  @override
+  State<AppExpansionCard> createState() => _AppExpansionCardState();
+}
+
+class _AppExpansionCardState extends State<AppExpansionCard> {
+  bool _isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+      child: AppCard(
+          child: ExpansionTile(
+        iconColor: Theme.of(context).colorScheme.onSurface,
+        title: AppText.labelLarge(widget.title),
+        trailing:
+            Icon(_isExpanded ? widget.collapsedIcon : widget.expandedIcon),
+        children: widget.children ?? [],
+        onExpansionChanged: (value) {
+          setState(() {
+            _isExpanded = value;
+          });
+        },
+      )),
+    );
+  }
+}
