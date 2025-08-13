df106957f04598e849c420cc5a6d824346a0b6fc
Austin Chang <austin.chang@belkin.com>
2025-08-13 14:56:00 +0800

    - add animate attr for mesh wired connection.dart

--- lib/widgets/lotties/mesh_wired_connection.dart
+++ lib/widgets/lotties/mesh_wired_connection.dart
@@ -5,17 +5,19 @@ import 'package:lottie/lottie.dart';
 class AppMeshWiredConnection extends StatelessWidget {
   final Size? size;
   final String? semanticLabel;
+  final bool? animate;
 
   const AppMeshWiredConnection({
     Key? key,
     this.size,
     this.semanticLabel,
+    this.animate,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Theme.of(context).colorScheme.onSurface.withAlpha(0xf0),
+      // color: Theme.of(context).colorScheme.onSurface.withAlpha(0x50),
       width: size?.width ?? 300,
       height: size?.height ?? 200,
       child: Semantics(
@@ -26,6 +28,7 @@ class AppMeshWiredConnection extends StatelessWidget {
             decoder: customDecoder,
             package: 'privacygui_widgets',
             fit: BoxFit.contain,
+            animate: animate,
           ),
         ),
       ),
